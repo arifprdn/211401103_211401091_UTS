8b849451e91fb7c0ea2f55396dca4f997295cfbe
arifprdn <119426536+arifprdn@users.noreply.github.com>
2023-05-05 20:20:31 +0700

    Add files via upload

---
+++ Restaurant_UTS_PBO_awal/src/Checkout.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Checkout {
+    static float totalOmset = Appetizer.harga;
+    static float totalBiayaProduksi = 0.25f * Appetizer.harga;
+    static float gajiKaryawan = 0.5f * totalBiayaProduksi;
+    static float pajak = 0.1f * totalOmset;
+    static float totalKeuntungan = (totalOmset - totalBiayaProduksi) - pajak - gajiKaryawan;
+public Checkout(){
+    System.out.println("Total Omset Penjualan: " + totalOmset);
+    System.out.println("Gaji Karyawan dan Biaya Fasilitas Pendukung: " + gajiKaryawan);
+    System.out.println("Biaya Produksi: " + totalBiayaProduksi);
+    System.out.println("Biaya Pajak: " + pajak);
+    System.out.println("Hasil Laba/Keuntungan Bersih : totalOmset - BiayaProduksi -  Pajak - Gaji karyawan dan Fasilitas pendukung = " + totalKeuntungan);
+    Main.Menu();
+}
+}
