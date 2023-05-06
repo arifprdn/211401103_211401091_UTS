ac79abd9217afd44d6c4b9bd1edba3d25b26f961
arifprdn <119426536+arifprdn@users.noreply.github.com>
2023-05-06 14:39:21 +0700

    Update Show.java

--- Restaurant_UTS_PBO/src/Show.java
+++ Restaurant_UTS_PBO/src/Show.java
@@ -1,6 +1,7 @@
 public class Show {
     public Show(){
-        System.out.println("Jumlah Pengunjung Diharapkan: " + (Checkout.totalKeuntungan * 0.25f));
+        float JumlahPengunjungDiharapkan = Checkout.totalOmset / Checkout.totalKeuntungan;
+        System.out.println("Jumlah Pengunjung Diharapkan: " + Math.round(JumlahPengunjungDiharapkan));
         System.out.println("Jumlah Pengunjung: " + Main.jumlahPengunjung);
         System.out.println("Total Keuntungan: " + Checkout.totalKeuntungan);
         Main.Menu();
