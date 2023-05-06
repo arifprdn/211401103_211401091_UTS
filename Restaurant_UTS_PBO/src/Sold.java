6081762be4e6a7272ac66c6f361f98a6395773ac
arifprdn <119426536+arifprdn@users.noreply.github.com>
2023-05-06 14:39:41 +0700

    Update Sold.java

--- Restaurant_UTS_PBO/src/Sold.java
+++ Restaurant_UTS_PBO/src/Sold.java
@@ -15,30 +15,40 @@ public class Sold {
     static int KuantitasPancake = 0;
     static int KuantitasWaffle = 0;
     static int KuantitasChurros = 0;
-    
+    static int KuantitasMojito = 0;
+    static int KuantitasBlueOcean = 0;
+    static int KuantitasLycheeBreeze = 0;
+    static int KuantitasColaFloat = 0;
+    static int KuantitasMilkTea = 0;
+    static int KuantitasOrangeJuice = 0;
     
 public Sold(){
     System.out.println("-----Jumlah Makanan dan Minuman yang terjual-----");
     System.out.println("------------------APPETIZER----------------------");
-    System.out.println("Salad       : "+KuantitasSalad);
-    System.out.println("Canape      : "+KuantitasCanape);
-    System.out.println("Pudding     : "+KuantitasPudding);
-    System.out.println("Gorengan    : "+KuantitasGorengan);
-    System.out.println("Dimsum      : "+KuantitasDimsum);
+    System.out.println("Salad           : "+KuantitasSalad);
+    System.out.println("Canape          : "+KuantitasCanape);
+    System.out.println("Pudding         : "+KuantitasPudding);
+    System.out.println("Gorengan        : "+KuantitasGorengan);
+    System.out.println("Dimsum          : "+KuantitasDimsum);
     System.out.println("-----------------MAIN COURSE---------------------");
-    System.out.println("Pizza       : "+KuantitasPizza);
-    System.out.println("Spagetti    : "+KuantitasSpagetti);
-    System.out.println("Burger      : "+KuantitasBurger);
-    System.out.println("Beef steak  : "+KuantitasBeefSteak);
-    System.out.println("Lasagna     : "+KuantitasLasagna);
+    System.out.println("Pizza           : "+KuantitasPizza);
+    System.out.println("Spagetti        : "+KuantitasSpagetti);
+    System.out.println("Burger          : "+KuantitasBurger);
+    System.out.println("Beef steak      : "+KuantitasBeefSteak);
+    System.out.println("Lasagna         : "+KuantitasLasagna);
     System.out.println("-------------------DESSERT-----------------------");
-    System.out.println("Ice Cream   : "+KuantitasIceCream);
-    System.out.println("Crepes      : "+KuantitasCrepes);
-    System.out.println("Pancake     : "+KuantitasPancake);
-    System.out.println("Waffle      : "+KuantitasWaffle);
-    System.out.println("Churos      : "+KuantitasChurros);
-    
-    
+    System.out.println("Ice Cream       : "+KuantitasIceCream);
+    System.out.println("Crepes          : "+KuantitasCrepes);
+    System.out.println("Pancake         : "+KuantitasPancake);
+    System.out.println("Waffle          : "+KuantitasWaffle);
+    System.out.println("Churos          : "+KuantitasChurros);
+    System.out.println("--------------------DRINK------------------------");
+    System.out.println("IMojito         : "+KuantitasMojito);
+    System.out.println("Blue Ocean      : "+KuantitasBlueOcean);
+    System.out.println("Lychee Breeze   : "+KuantitasLycheeBreeze);
+    System.out.println("Cola Float      : "+KuantitasColaFloat);
+    System.out.println("Milk Tea        : "+KuantitasMilkTea);   
+    System.out.println("Orange Juice    : "+KuantitasOrangeJuice);
     Main.Menu();
 }
 }
