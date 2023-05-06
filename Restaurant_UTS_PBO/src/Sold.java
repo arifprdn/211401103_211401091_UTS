74f0396fe1dc7b897b63221aa9b2f96ecfd49047
YAKIN OTNIEL SURBAKTI <119327301+yakinotniel@users.noreply.github.com>
2023-05-06 10:20:16 +0700

    Update Sold.java

--- Restaurant_UTS_PBO/src/Sold.java
+++ Restaurant_UTS_PBO/src/Sold.java
@@ -5,13 +5,39 @@ public class Sold {
     static int KuantitasPudding = 0;
     static int KuantitasGorengan = 0;
     static int KuantitasDimsum = 0;
+    static int KuantitasPizza = 0;
+    static int KuantitasSpagetti = 0;
+    static int KuantitasBurger = 0;
+    static int KuantitasBeefSteak = 0;
+    static int KuantitasLasagna = 0;
+    static int KuantitasIceCream = 0;
+    static int KuantitasCrepes = 0;
+    static int KuantitasPancake = 0;
+    static int KuantitasWaffle = 0;
+    static int KuantitasChurros = 0;
+    
+    
 public Sold(){
     System.out.println("-----Jumlah Makanan dan Minuman yang terjual-----");
-    System.out.println("Salad : "+KuantitasSalad);
-    System.out.println("Canape : "+KuantitasCanape);
-    System.out.println("Pudding : "+KuantitasPudding);
-    System.out.println("Gorengan : "+KuantitasGorengan);
-    System.out.println("Dimsum : "+KuantitasDimsum);
+    System.out.println("------------------APPETIZER----------------------");
+    System.out.println("Salad       : "+KuantitasSalad);
+    System.out.println("Canape      : "+KuantitasCanape);
+    System.out.println("Pudding     : "+KuantitasPudding);
+    System.out.println("Gorengan    : "+KuantitasGorengan);
+    System.out.println("Dimsum      : "+KuantitasDimsum);
+    System.out.println("-----------------MAIN COURSE---------------------");
+    System.out.println("Pizza       : "+KuantitasPizza);
+    System.out.println("Spagetti    : "+KuantitasSpagetti);
+    System.out.println("Burger      : "+KuantitasBurger);
+    System.out.println("Beef steak  : "+KuantitasBeefSteak);
+    System.out.println("Lasagna     : "+KuantitasLasagna);
+    System.out.println("-------------------DESSERT-----------------------");
+    System.out.println("Ice Cream   : "+KuantitasIceCream);
+    System.out.println("Crepes      : "+KuantitasCrepes);
+    System.out.println("Pancake     : "+KuantitasPancake);
+    System.out.println("Waffle      : "+KuantitasWaffle);
+    System.out.println("Churos      : "+KuantitasChurros);
+    
     
     Main.Menu();
 }
