210e57040c9ec098f57cfe1f9d20488747c10d00
YAKIN OTNIEL SURBAKTI <119327301+yakinotniel@users.noreply.github.com>
2023-05-06 10:19:51 +0700

    Update Show.java

--- Restaurant_UTS_PBO/src/Show.java
+++ Restaurant_UTS_PBO/src/Show.java
@@ -1,6 +1,6 @@
 public class Show {
     public Show(){
-        System.out.println("Jumlah Pengunjung Diharapkan: " + Appetizer.harga / Checkout.totalKeuntungan);
+        System.out.println("Jumlah Pengunjung Diharapkan: " + (Checkout.totalKeuntungan * 0.25f));
         System.out.println("Jumlah Pengunjung: " + Main.jumlahPengunjung);
         System.out.println("Total Keuntungan: " + Checkout.totalKeuntungan);
         Main.Menu();
